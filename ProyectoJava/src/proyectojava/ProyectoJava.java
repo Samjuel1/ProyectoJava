4dd0b81655814dcea8c735f8c31f72b0ade6f0b6
Samjuel1 <samuel.garciadios@edu.uah.es>
2025-04-24 13:27:55 +0200

    pequeña actualizacion cliente vip

--- ProyectoJava/src/proyectojava/ProyectoJava.java
+++ ProyectoJava/src/proyectojava/ProyectoJava.java
@@ -16,6 +16,8 @@ public class ProyectoJava {
     public static void main(String[] args) {
         Cliente cliente1 = new Cliente("hola@gmail","1234","Juan","651728694","1522 2727 2833 2387","12/28", "callef",4,"madrid",28732,true);
         System.out.println(cliente1);
+        Administrador admin1 = new Administrador("admin@gmail.com","admin123");
+        System.out.println(admin1);
     }
     
 }
